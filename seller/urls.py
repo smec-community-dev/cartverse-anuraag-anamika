4294781540ad46785a7c1d52cbf5b84a6924cafa
ksanuraag <ksanuraag@gmail.com>
2025-11-21 22:05:47 +0530

    reviewchange2comma

--- seller/urls.py
+++ seller/urls.py
@@ -15,7 +15,7 @@ urlpatterns=[
     path('single_orderproduct/<slug:slug>/',views.single_order_product,name='single_order'),
     path('seller_profile/',views.seller_profile,name='seller_profile'),
     path('seller_change_password/',views.change_password,name='change_password'),
-    path('seller_delete/',views.seller_account_delete,name='seller_account_delete')
+    path('seller_delete/',views.seller_account_delete,name='seller_account_delete'),
     path('seller_profile/',views.seller_profile,name='seller_profile'),
     path('seller_review/<int:product_id>/<slug:slug>/',views.seller_review,name='seller_review')
 ]
