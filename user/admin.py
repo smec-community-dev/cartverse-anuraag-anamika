b0088359d4b7763e538eea13e47c66e965bb7dcb
ksanuraag <ksanuraag@gmail.com>
2025-11-22 18:26:12 +0530

    admin change

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Customer,Cart,Wishlist,Review,Order,OrderItem,Address
+from .models import Customer,Cart,Wishlist,Review,Order,OrderItem,Address,ReviewImage
 
 admin.site.register(Customer)
 admin.site.register(Cart)
@@ -9,3 +9,4 @@ admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Address)
+admin.site.register(ReviewImage)
