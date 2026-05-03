1218451c663139175c91cd5f0d900e3f75614598
ksanuraag <ksanuraag@gmail.com>
2026-05-03 16:56:30 +0530

    added https1

--- project/settings.py
+++ project/settings.py
@@ -81,9 +81,11 @@ CHANNEL_LAYERS = {
     }
 }
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+USE_X_FORWARDED_HOST = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
